3d1f642389b2c3d2fb6721f5b703b0c20e36248c
Tom Stoepker <tom.stoepker@gmail.com>
2025-03-01 16:03:31 +0100

    task: bootstrap the venture time bevy project

--- venture_time/src/main.rs
+++ venture_time/src/main.rs
@@ -1,7 +1,15 @@
-use bevy::{app::App, DefaultPlugins};
-
-
+use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new().add_plugins(
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Venture Time".to_string(),
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        })
+    ).run();
 }
